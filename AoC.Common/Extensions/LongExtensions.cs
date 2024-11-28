e8093c07ab4a4ecddd1ed98643d0011c9daf3301
Edwin Croes <ec.croes@belastingdienst.nl>
2024-11-28 16:51:14 +0100

    Setup of the project

---
+++ AoC.Common/Extensions/LongExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC.Common.Extensions;
+
+public static class LongExtensions
+{
+    public static int GetNthDigit(this long value, int n) =>
+        (int)(value / (long)Math.Pow(10, n) % 10);
+
+    public static long SetNthDigit(this long value, int n, int to) =>
+         value + (to - value.GetNthDigit(n)) * (long)Math.Pow(10, n);
+
+    public static bool IsEven(this long value) =>
+        value % 2 == 0;
+}
