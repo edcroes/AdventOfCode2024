047f71c5f1a32b086f44dd9519dd64e9d114e5a1
Edwin Croes <ec.croes@belastingdienst.nl>
2024-12-13 23:46:36 +0100

    Day 09, Faster compacting and checksumming

--- AoC2024/Day09/Day09.cs
+++ AoC2024/Day09/Day09.cs
@@ -28,17 +28,29 @@ public class Day09 : IMDay
     public async Task<string> GetAnswerPart2()
     {
         var input = await GetInput();
-        List<Block> drive = [];
+        
+        List<Block> files = [];
+        List<Block> freeSpace = [];
 
         var id = 0;
+        var index = 0;
+
         for (var i = 0; i < input.Length; i++)
         {
-            drive.Add(new(i.IsEven() ? id++ : -1, input[i]));
+            if (input[i] > 0)
+            {
+                if (i.IsEven())
+                    files.Add(new(id++, input[i]) { Index = index });
+                else
+                    freeSpace.Add(new(-1, input[i]) { Index = index });
+            }
+
+            index += input[i];
         }
 
-        CompactDrive(drive);
+        CompactDrive(files, freeSpace);
 
-        return ComputeChecksum(drive).ToString();
+        return files.Sum(f => f.ComputeChecksum()).ToString();
     }
 
     private static void CompactDrive(List<int> drive)
@@ -63,37 +75,28 @@ public class Day09 : IMDay
         }
     }
 
-    private static void CompactDrive(List<Block> drive)
+    private static void CompactDrive(List<Block> files, List<Block> freeSpace)
     {
-        HashSet<int> movedBlocks = [-1];
-
-        for (var i = drive.Count - 1; i >= 0; i--)
+        files.Reverse();
+        foreach (var file in files)
         {
-            var block = drive[i];
-            if (movedBlocks.Contains(block.Id))
-                continue;
+            if (freeSpace.Count == 0 || freeSpace[0].Index > file.Index)
+                break;
 
-            var emptyBlock = drive.FirstOrDefault(b => b.Id == -1 && b.Length >= block.Length);
+            var freeBlock = freeSpace.FirstOrDefault(f => f.Length >= file.Length);
 
-            if (emptyBlock != default)
+            if (freeBlock is not null && freeBlock.Index < file.Index)
             {
-                var emptyIndex = drive.IndexOf(emptyBlock);
-                if (emptyIndex < i)
-                {
-                    drive.RemoveAt(i);
-                    drive.RemoveAt(emptyIndex);
-                    drive.Insert(emptyIndex, block);
-                    drive.Insert(i, new(-1, block.Length));
+                file.Index = freeBlock.Index;
 
-                    if (block.Length < emptyBlock.Length)
-                    {
-                        drive.Insert(emptyIndex + 1, new(-1, emptyBlock.Length - block.Length));
-                        i++;
-                    }
+                var indexOfFreeBlock = freeSpace.IndexOf(freeBlock);
+                freeSpace.Remove(freeBlock);
+
+                if (freeBlock.Length > file.Length)
+                {
+                    freeSpace.Insert(indexOfFreeBlock, new(-1, freeBlock.Length - file.Length) { Index = freeBlock.Index + file.Length });
                 }
             }
-
-            movedBlocks.Add(block.Id);
         }
     }
 
@@ -101,46 +104,14 @@ public class Day09 : IMDay
         Enumerable.Range(0, drive.Count)
             .Sum(b => drive[b] == -1 ? 0L : drive[b] * b);
 
-    private static long ComputeChecksum(List<Block> drive)
-    {
-        List<int> realDrive = [];
-
-        foreach (var block in drive)
-        {
-            for (var i = 0; i < block.Length; i++)
-            {
-                realDrive.Add(block.Id);
-            }
-        }
-
-        return ComputeChecksum(realDrive);
-    }
-
-    private static void DumpDrive(IEnumerable<int> drive)
-    {
-        foreach (var item in drive)
-        {
-            Console.Write(item == -1 ? "." : item);
-        }
-
-        Console.WriteLine();
-    }
-
-    private static void DumpDrive(IEnumerable<Block> drive)
-    {
-        foreach (var item in drive)
-        {
-            for (var i = 0; i < item.Length; i++)
-            {
-                Console.Write(item.Id == -1 ? "." : item.Id);
-            }
-        }
-
-        Console.WriteLine();
-    }
-
     private async Task<int[]> GetInput() =>
         (await FileParser.ReadLinesAsIntArray(FilePath))[0];
 
-    private record struct Block(int Id, int Length);
+    private record Block(int Id, int Length)
+    {
+        public int Index { get; set; } = -1;
+
+        public long ComputeChecksum() =>
+            (long)((double)(Index * 2 + Length - 1) / 2 * Id * Length);
+    }
 }
